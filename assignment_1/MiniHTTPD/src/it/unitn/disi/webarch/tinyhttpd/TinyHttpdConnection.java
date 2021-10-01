0c5a47c5bc5d4fb115294ac7f29bd14f486e7b64
Marcel Stolin <marcelstolin@gmail.com>
2021-10-01 17:52:37 +0200

    Finished task

--- assignment_1/MiniHTTPD/src/it/unitn/disi/webarch/tinyhttpd/TinyHttpdConnection.java
+++ assignment_1/MiniHTTPD/src/it/unitn/disi/webarch/tinyhttpd/TinyHttpdConnection.java
@@ -6,10 +6,12 @@ import java.util.StringTokenizer;
 
 class TinyHttpdConnection extends Thread {
 
-    Socket sock;
+    private Socket sock;
+    private PrintStream ps;
+    private OutputStream out;
 
     TinyHttpdConnection(Socket s) {
-        sock = s;
+        this.sock = s;
         setPriority(NORM_PRIORITY - 1);
         start();
     }
@@ -17,15 +19,15 @@ class TinyHttpdConnection extends Thread {
     @Override
     public void run() {
         System.out.println("=========");
-        System.out.println("Connected on port "+ sock.getPort());
-        OutputStream out = null;
+        System.out.println("Connected on port "+ this.sock.getPort());
+        this.out = null;
         try {
             // OPEN SOCKETS FOR READING AND WRITING
             BufferedReader d =
                     new BufferedReader(new InputStreamReader(
-                            sock.getInputStream()));
-            out = sock.getOutputStream();
-            PrintStream ps=new PrintStream(out);
+                            this.sock.getInputStream()));
+            this.out = this.sock.getOutputStream();
+            this.ps = new PrintStream(out);
             // READ REQUEST
             String req = d.readLine();
             if (req==null) return;
@@ -36,33 +38,60 @@ class TinyHttpdConnection extends Thread {
                 head = d.readLine();
                 System.out.println("Header: " + head);
             } while (head!=null && head.length()>0);
+
             // PARSE REQUEST
             StringTokenizer st = new StringTokenizer(req);
             if ((st.countTokens() >= 2) && st.nextToken().equals("GET")) {
-                if ((req = st.nextToken()).startsWith("/")) {
-                    req = req.substring(1);
+                // GANZ WICHTIG: Erstmal URL in seine EinzelTeile PATH, QUERY
+                req = st.nextToken();
+                String path;
+                String query = "";
+                String[] splittedURL = req.split("\\?");
+                path = splittedURL[0];
+                if (path.equals("/")) {
+                    path = path + "index.html";
                 }
-                if (req.endsWith("/") || req.equals("")) {
-                    req = req + "index.html";
+                if (splittedURL.length > 1) {
+                    query = splittedURL[1];
                 }
-                // OPEN REQUESTED FILE AND COPY IT TO CLIENT
-                try {
-                    //All our requested files must be in the "Documents" directory
-                    FileInputStream fis = new FileInputStream("Documents/"+req);
-                    int responseLength=fis.available();
-                    // LET'S SEND THE RESPONSE HEADERS
-                    ps.print("HTTP/1.1 200 OK\r\n");
-                    ps.print("Content-Length: "+responseLength+"\r\n");
-                    ps.print("Content-Type: text/html\r\n");
-                    ps.print("\r\n");
-                    // LET'S SEND THE CONTENT
-                    byte[] data = new byte[responseLength];
-                    fis.read(data);
-                    out.write(data);
-                    fis.close();
-                } catch (FileNotFoundException e) {
-                    ps.print("HTTP/1.1 404 Not Found \r\n\r\n");
-                    System.out.println("404 Not Found: " + req);
+                System.out.println("Path: " + path);
+                System.out.println("Query: " + query);
+
+                StringTokenizer pathTokenizer = new StringTokenizer(path, "/");
+                path = pathTokenizer.nextToken();
+
+                if (path.equals("process") && pathTokenizer.hasMoreTokens()) {
+                    // user wants to start a java process
+                    String processName = pathTokenizer.nextToken();
+
+                    if (!pathTokenizer.hasMoreTokens()) {
+                        // Path is a valid process name
+                        System.out.println("Java process name: " + processName);
+
+                        try {
+                            String processOutput = this.launchJavaProcess(processName, query);
+                            System.out.println("OUTPUT IS: " + processOutput);
+                            this.sendResponseHeader(processOutput.length());
+                            ps.print(processOutput);
+                        } catch (Exception e) {
+                            System.out.println("Error: " + e.getMessage());
+                            ps.print("HTTP/1.1 400 Bad Request\r\n\r\n");
+                            System.out.println("400 Bad Request: " + req);
+                        }
+                    } else {
+                        System.out.println("Error: The path " + req + " has too many tokens.");
+                        // SEND ERROR RESPONSE
+                        ps.print("HTTP/1.1 400 Bad Request\r\n\r\n");
+                        System.out.println("400 Bad Request: " + req);
+                    }
+                } else {
+                    // OPEN REQUESTED FILE AND COPY IT TO CLIENT
+                    try {
+                        this.sendResponse(req);
+                    } catch (FileNotFoundException e) {
+                        ps.print("HTTP/1.1 404 Not Found \r\n\r\n");
+                        System.out.println("404 Not Found: " + req);
+                    }
                 }
             } else {
                 ps.print("HTTP/1.1 400 Bad Request\r\n\r\n");
@@ -73,11 +102,80 @@ class TinyHttpdConnection extends Thread {
         } finally { // the following code is ALWAYS executed
             try {
                 // let's close all channels
-                out.close();
-                sock.close();
+                this.out.close();
+                this.sock.close();
             } catch (IOException ex) {
                 System.out.println("I/O error on close" + ex);
             }
         }
     }
+
+    private String launchJavaProcess(String processName, String query) throws Exception {
+        String command;
+
+        if (processName.equals("reverse")) {
+            command = this.generateReverseProcessCommand(query);
+        } else {
+            // This process is not known
+            throw new Exception("No process with the name \"" + processName + "\" known");
+        }
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("bash", "-c", command);
+        Process process = processBuilder.start();
+
+        StringBuilder output = new StringBuilder();
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            output.append(line + "\n");
+        }
+
+        return output.toString();
+    }
+
+    private String generateReverseProcessCommand(String query) throws Exception {
+        StringTokenizer paramTokenizer = new StringTokenizer(query, "&");
+        // first check if there are any parameters
+        if (!paramTokenizer.hasMoreTokens()) {
+            throw new Exception("No parameters given. The reverse process needs at least one parameter");
+        }
+        String firstParam = paramTokenizer.nextToken();
+        String[] paramKeyValue = firstParam.split("=");
+        if (paramKeyValue.length >= 2) {
+            // We need at least 2 elements
+            String textToReverse = paramKeyValue[1];
+            String classPath = "/Users/marcel/workspace/web-architectures/assignment_1/StringReverser/out/production/StringReverser";
+            String className = "it.unitn.disi.webarch.assignment1.StringReverser";
+            String command = "java" + " -cp " + classPath + " " + className + " " + textToReverse;
+
+            return command;
+        } else {
+            System.out.println();
+            throw new Exception("No valid parameter input given for query \"" + query + "\"");
+        }
+    }
+
+    private void sendResponse(String request) throws java.io.IOException {
+        //All our requested files must be in the "Documents" directory
+        FileInputStream fis = new FileInputStream("Documents/" + request);
+        int responseLength = fis.available();
+        // LET'S SEND THE RESPONSE HEADERS
+        this.sendResponseHeader(responseLength);
+        // LET'S SEND THE CONTENT
+        byte[] data = new byte[responseLength];
+        fis.read(data);
+        out.write(data);
+        fis.close();
+    }
+
+    private void sendResponseHeader(int responseLength) {
+        ps.print("HTTP/1.1 200 OK\r\n");
+        ps.print("Content-Length: " + responseLength + "\r\n");
+        ps.print("Content-Type: text/html\r\n");
+        ps.print("\r\n");
+    }
+
 }
