729d548bc2387e19c3f3478ac85e92b8ea9058b4
Marcel Stolin <marcelstolin@gmail.com>
2021-10-05 21:34:40 +0200

    Created the login, user, and room servlet

---
+++ assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/controller/UserPageServlet.java
@@ -0,0 +1,19 @@
+package it.unitn.disi.webarch.chat.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "UserPageServlet", value = "/UserPageServlet")
+public class UserPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
