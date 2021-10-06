30c90beeb01026f7e2be023d87213e408d506603
Marcel Stolin <marcelstolin@gmail.com>
2021-10-06 09:29:33 +0200

    Added authentication filter

---
+++ assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/controller/AuthenticationFilter.java
@@ -0,0 +1,29 @@
+package it.unitn.disi.webarch.chat.controller;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthenticationFilter")
+public class AuthenticationFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        boolean isAuthenticated = false;
+
+        if (isAuthenticated) {
+            // go on as expected
+            chain.doFilter(request, response);
+        } else {
+            // forward to login
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/login");
+            dispatcher.forward(request, response);
+        }
+
+    }
+}
