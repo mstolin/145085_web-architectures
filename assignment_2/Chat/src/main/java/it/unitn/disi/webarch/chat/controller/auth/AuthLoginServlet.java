b68446be0c72ff56a028e3f27b53db72d8a6487a
Marcel Stolin <marcelstolin@gmail.com>
2021-10-10 17:39:51 +0200

    Read users from file

--- assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/controller/auth/AuthLoginServlet.java
+++ assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/controller/auth/AuthLoginServlet.java
@@ -2,7 +2,7 @@ package it.unitn.disi.webarch.chat.controller.auth;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,12 +11,6 @@ public class AuthLoginServlet extends HttpServlet {
     private final String SESSION_KEY_USERNAME = "username";
     private final String SESSION_KEY_IS_AUTHENTICATED = "is_authenticated";
 
-    private Map<String, String> authorizedUsers = new HashMap<String, String>() {{
-        put("user1", "pw1");
-        put("user2", "pw2");
-        put("admin", "root");
-    }};
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
@@ -24,9 +18,14 @@ public class AuthLoginServlet extends HttpServlet {
 
         System.out.println("Login attempt for user " + username);
 
+        String userFilePath = this.getClass().getClassLoader().getResource("users.txt").getPath();
+        System.out.println("Read users from " + userFilePath);
+        File userFile = new File(userFilePath);
+        Map<String, String> authorizedUsers = this.readUsersFromFile(userFile);
+
         if (username != null && password != null) {
-            if (this.authorizedUsers.containsKey(username) || username.equals("admin")) {
-                String userPassword = this.authorizedUsers.get(username);
+            if (authorizedUsers.containsKey(username) || username.equals("admin")) {
+                String userPassword = authorizedUsers.get(username);
 
                 if (userPassword.equals(password)) {
                     // user is authenticated
@@ -40,7 +39,7 @@ public class AuthLoginServlet extends HttpServlet {
                     response.sendRedirect(request.getContextPath() + "/login");
                 }
             } else {
-                // The user does not exists -> Back to login
+                // The user does not exist -> Back to login
                 System.out.println("ERROR (AuthLogin) - User '" + username + "' does not exist");
                 response.sendRedirect(request.getContextPath() + "/login");
             }
@@ -54,6 +53,40 @@ public class AuthLoginServlet extends HttpServlet {
     private void updateUserAuthStatus(HttpSession session, String username, boolean isAuthenticated) {
         session.setAttribute(SESSION_KEY_IS_AUTHENTICATED, isAuthenticated);
         session.setAttribute(SESSION_KEY_USERNAME, username);
+    }
+
+    private Map<String, String> readUsersFromFile(File userFile) {
+        Map<String, String> userMap = new HashMap();
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            fileReader = new FileReader(userFile);
+            bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] userAndPw = line.split(":");
+                if (userAndPw.length >= 2) {
+                    String user = userAndPw[0].trim();
+                    String password = userAndPw[1].trim();
+
+                    userMap.put(user, password);
+                }
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println(exception.getMessage());
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        } finally {
+            try {
+                if (bufferedReader != null) bufferedReader.close();
+                if (fileReader != null) fileReader.close();
+            } catch (IOException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+
+        return userMap;
     }
 
 }
