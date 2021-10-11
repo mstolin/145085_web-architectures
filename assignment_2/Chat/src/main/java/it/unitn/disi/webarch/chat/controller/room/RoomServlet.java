dc3b7b0b3dbb534bb1f6a156322882feb0a16b48
Marcel Stolin <marcelstolin@gmail.com>
2021-10-11 16:35:13 +0200

    Moved RoomServlet

--- assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/controller/room/RoomServlet.java
+++ assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/controller/room/RoomServlet.java
@@ -4,12 +4,18 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "RoomServlet", value = "/RoomServlet")
 public class RoomServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
 
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1> ROOM SERVLET </h1>");
+        out.println("</body></html>");
     }
 
     @Override
