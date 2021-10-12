298c6aaa5ee845a884d8c26a21b9a384fe314738
Marcel Stolin <marcelstolin@gmail.com>
2021-10-12 08:51:24 +0200

    Updated roomstore to use the custom class

--- assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/helper/RoomStore.java
+++ assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/helper/RoomStore.java
@@ -1,21 +1,25 @@
 package it.unitn.disi.webarch.chat.helper;
 
+import it.unitn.disi.webarch.chat.models.room.Room;
+
 import java.util.*;
 
 public class RoomStore {
 
-    private static Set<String> roomStore = new HashSet<String>();
+    private static Set<Room> roomStore = new HashSet<Room>();
 
     public static void addRoom(String roomName) {
-        roomStore.add(roomName);
+        Room room = new Room(roomName);
+        roomStore.add(room);
     }
 
-    public static Set<String> getAllRooms() {
+    public static Set<Room> getAllRooms() {
         return roomStore;
     }
 
     public static boolean hasRoom(String roomName) {
-        return roomStore.contains(roomName);
+        Room room = new Room(roomName);
+        return roomStore.contains(room);
     }
 
 }
