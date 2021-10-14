c188dea280c5fdad58748014df839f0685a7c609
Marcel Stolin <marcelstolin@gmail.com>
2021-10-14 10:50:16 +0200

    The admin can now add users

--- assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/controller/admin/AdminServlet.java
+++ assignment_2/Chat/src/main/java/it/unitn/disi/webarch/chat/controller/admin/AdminServlet.java
@@ -1,5 +1,8 @@
 package it.unitn.disi.webarch.chat.controller.admin;
 
+import it.unitn.disi.webarch.chat.helper.UserStore;
+import it.unitn.disi.webarch.chat.models.user.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
@@ -14,6 +17,35 @@ public class AdminServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
 
+        if (username != null && password != null) {
+            // valid request
+            if (this.areUsernamePasswordValid(username, password)) {
+                // valid credentials, add user
+                User user = new User(username, password);
+                UserStore.getInstance().add(user);
+
+                try {
+                    UserStore.getInstance().addUser(user);
+                } catch (Exception exception) {
+                    System.out.println("ERROR (AdminServlet) - " + exception.getMessage());
+                } finally {
+                    // forward
+                    this.doGet(request, response);
+                }
+            } else {
+                // invalid credentials -> reload page
+                this.doGet(request, response);
+            }
+        } else {
+            // invalid request
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    private boolean areUsernamePasswordValid(String username, String password) {
+        return username.length() >= 1 && password.length() >= 1;
     }
 }
