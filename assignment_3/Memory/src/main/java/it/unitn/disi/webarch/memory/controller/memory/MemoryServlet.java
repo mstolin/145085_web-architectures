ab5af8a69101b2e04d5c31f55a3fcf9e8c35fb5a
Marcel Stolin <marcelstolin@gmail.com>
2021-10-23 23:01:12 +0200

    Updated the memory game page

--- assignment_3/Memory/src/main/java/it/unitn/disi/webarch/memory/controller/memory/MemoryServlet.java
+++ assignment_3/Memory/src/main/java/it/unitn/disi/webarch/memory/controller/memory/MemoryServlet.java
@@ -1,10 +1,21 @@
 package it.unitn.disi.webarch.memory.controller.memory;
 
+import it.unitn.disi.webarch.memory.models.Scoreboard;
+import it.unitn.disi.webarch.memory.models.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class MemoryServlet extends HttpServlet {
+
+    private final String REQUEST_KEY_USERNAME = "username";
+    private final String REQUEST_KEY_POINTS = "points";
+    private final String SERVLET_KEY_SCOREBOARD = "scoreboard";
+
+    private Logger logger = Logger.getLogger(this.getClass().getSimpleName());
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.getServletContext()
@@ -14,6 +25,32 @@ public class MemoryServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter(REQUEST_KEY_USERNAME);
+        int points = Integer.parseInt(request.getParameter(REQUEST_KEY_POINTS));
 
+        if (this.areParametersValid(username, points)) {
+            ServletContext context = request.getServletContext();
+            User user = new User(username);
+            this.logger.info("Updated score for user " + username);
+            this.setScore(context, user, points);
+        } else {
+            this.logger.warning("POST Request with username " + username + " and points" + points + " was not successful");
+            response.sendError(400);
+        }
     }
+
+    private boolean areParametersValid(String username, int points) {
+        return username != null && username.length() >= 1 && points >= 0;
+    }
+
+    private void setScore(ServletContext context, User user, Integer points) {
+        Scoreboard scoreboard = (Scoreboard) context.getAttribute(SERVLET_KEY_SCOREBOARD);
+        if (scoreboard == null) {
+            scoreboard = new Scoreboard();
+        }
+
+        scoreboard.addUserScore(user, points);
+        context.setAttribute(SERVLET_KEY_SCOREBOARD, scoreboard);
+    }
+
 }
