7499ecc8c0f35cf86eaaf95c72a5024a967d9468
Marcel Stolin <marcelstolin@gmail.com>
2021-10-26 09:31:29 +0200

    Able to parse POST requests

--- assignment_3/Memory/src/main/java/it/unitn/disi/webarch/memory/controller/ranking/RankingServlet.java
+++ assignment_3/Memory/src/main/java/it/unitn/disi/webarch/memory/controller/ranking/RankingServlet.java
@@ -5,14 +5,17 @@ import it.unitn.disi.webarch.memory.models.User;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 public class RankingServlet extends HttpServlet {
 
-    private final String REQUEST_KEY_USERNAME = "username";
     private final String REQUEST_KEY_POINTS = "points";
     private final String SERVLET_KEY_SCOREBOARD = "scoreboard";
+    private final String SESSION_KEY_USER = "SESSION_KEY_USER";
 
     private Logger logger = Logger.getLogger(this.getClass().getSimpleName());
 
@@ -25,22 +28,59 @@ public class RankingServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter(REQUEST_KEY_USERNAME);
+        HttpSession httpSession = request.getSession();
+        User user = (User) httpSession.getAttribute(SESSION_KEY_USER);
         int points = Integer.parseInt(request.getParameter(REQUEST_KEY_POINTS));
 
-        if (this.areParametersValid(username, points)) {
+        logger.info("POST request received for user " + user.getName() + " and points " + points);
+
+        if (this.areParametersValid(user, points)) {
             ServletContext context = request.getServletContext();
-            User user = new User(username);
-            this.logger.info("Updated score for user " + username);
+            this.logger.info("Update score for user " + user.getName());
             this.setScore(context, user, points);
+            response.setStatus(HttpServletResponse.SC_OK);
         } else {
-            this.logger.warning("POST Request with username " + username + " and points" + points + " was not successful");
-            response.sendError(400);
+            this.logger.warning("POST Request with username " + user.getName() + " and points" + points + " was not successful");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
 
-    private boolean areParametersValid(String username, int points) {
-        return username != null && username.length() >= 1 && points >= 0;
+    private boolean areParametersValid(User user, int points) {
+        return user != null && points >= 0;
+    }
+
+    private String getBodyAsString(HttpServletRequest request)  {
+        String body = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            // throw ex;
+            return "";
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+
+                }
+            }
+        }
+
+        body = stringBuilder.toString();
+        return body;
     }
 
     private void setScore(ServletContext context, User user, Integer points) {
