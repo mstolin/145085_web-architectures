6be576e41efc96887ff41be44070fc19ccd7a291
Marcel Stolin <marcelstolin@gmail.com>
2021-11-14 12:34:21 +0100

    Mocked dummy members

--- assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/app.component.ts
+++ assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Member} from "./models/member";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'parliament';
+
+  members: Member[] = [
+      new Member(
+        1735,
+        "Constance, Angela",
+        "1970-07-15T00:00:00",
+        "http://scottishparliament.thirdlight.com/file/35820129812",
+        [],
+        []
+      ),
+      new Member(
+        1741,
+        "Goldie, Annabel",
+        "",
+        "",
+        [],
+        []
+      ),
+      new Member(
+        1742,
+        "Ewing, Annabelle",
+        "1960-08-20T00:00:00",
+        "http://scottishparliament.thirdlight.com/file/35960261933",
+        [],
+        []
+      )
+  ];
+
 }
--- assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/app.module.ts
+++ assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MemberCardComponent } from './components/member-card/member-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
