0a109e3d66e5342d8136b8754ce7bb422319c791
Marcel Stolin <marcelstolin@gmail.com>
2021-11-14 21:25:38 +0100

    Added routes for list and detail

--- assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/app-routing.module.ts
+++ assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MemberListComponent} from "./components/member-list/member-list.component";
+import {MemberDetailComponent} from "./components/member-detail/member-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MemberListComponent },
+  { path: 'detail', component: MemberDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
