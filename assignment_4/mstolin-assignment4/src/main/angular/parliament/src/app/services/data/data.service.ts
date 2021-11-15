d2b3ad0a1594703f675a87dd35bf8d6588e20c9c
Marcel Stolin <marcelstolin@gmail.com>
2021-11-15 22:02:50 +0100

    Extended data service
    Fetch member parties

--- assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/services/data/data.service.ts
+++ assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/services/data/data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {Member} from "../../models/member";
 import {HttpClient} from "@angular/common/http";
 import {forkJoin} from "rxjs";
+import {MemberParty} from "../../models/member-party";
+import {DataResponse} from "../../models/data-response";
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +11,32 @@ import {forkJoin} from "rxjs";
 export class DataService {
 
   private membersApiUrl: string = 'https://data.parliament.scot/api/members';
+  private memberPartiesApiUrl: string = 'https://data.parliament.scot/api/memberparties';
 
   private _members: Member[] = [];
+  private _memberParties: MemberParty[] = [];
 
   constructor(private http: HttpClient) { }
 
   private isDataAlreadyFetched(): boolean {
-    return this._members.length > 0;
+    return this._members.length > 0 && this._memberParties.length > 0;
   }
 
-  public fetchData(): Promise<Member[]> {
+  public fetchData(): Promise<DataResponse> {
     if (!this.isDataAlreadyFetched()) {
       let membersRequest = this.http.get<Member[]>(this.membersApiUrl);
+      let memberPartyRequest = this.http.get<MemberParty[]>(this.memberPartiesApiUrl);
 
-      let promise = new Promise<Member[]>((resolve, reject) => {
-        forkJoin([membersRequest])
+      let promise = new Promise<DataResponse>((resolve, reject) => {
+        forkJoin([membersRequest, memberPartyRequest])
           .subscribe(responses => {
             let members: Member[] = responses[0];
+            let memberParties: MemberParty[] = responses[1];
+
             this._members = members;
-            resolve(members)
+            this._memberParties = memberParties;
+
+            resolve({members, memberParties})
           }, error => {
             reject(error);
           });
@@ -35,15 +44,18 @@ export class DataService {
 
       return promise;
     } else {
-      return new Promise<Member[]>((resolve, _) => {
-        resolve(this._members);
+      return new Promise<DataResponse>((resolve, _) => {
+        resolve({members: this._members, memberParties: this._memberParties});
       });
     }
   }
 
-
   get members(): Member[] {
     return this._members;
+  }
+
+  get memberParties(): MemberParty[] {
+    return this._memberParties;
   }
 
 }
