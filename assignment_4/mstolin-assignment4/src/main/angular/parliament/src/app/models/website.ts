650d0ee049e7ff76dedeef9289c537f57e81d60b
Marcel Stolin <marcelstolin@gmail.com>
2021-11-23 16:52:13 +0100

    Updated website model

--- assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/models/website.ts
+++ assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/models/website.ts
@@ -1,2 +1,27 @@
+import {WebsiteResponse} from "./responses/website-response";
+
 export class Website {
+
+  private readonly _id: number;
+  private readonly _personId: number;
+  private readonly _url: string;
+
+  constructor(websiteResponse: WebsiteResponse) {
+    this._id = websiteResponse.ID;
+    this._personId = websiteResponse.PersonID;
+    this._url = websiteResponse.WebURL;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get personId(): number {
+    return this._personId;
+  }
+
+  get url(): string {
+    return this._url;
+  }
+
 }
