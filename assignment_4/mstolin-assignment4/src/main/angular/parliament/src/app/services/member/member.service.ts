b627bfaec825bb812def4a336c131cc670241f30
Marcel Stolin <marcelstolin@gmail.com>
2021-11-23 16:38:35 +0100

    Updated member service
    Map to member model

--- assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/services/member/member.service.ts
+++ assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/services/member/member.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {MemberResponse} from "../../models/responses/member-response";
+import {map, Observable} from "rxjs";
 import {Member} from "../../models/member";
-import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
 
-  private membersApiUrl: string = 'https://data.parliament.scot/api/members';
+  private readonly membersApiUrl: string = 'https://data.parliament.scot/api/members';
 
   constructor(private http: HttpClient) { }
 
   public fetchData(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.membersApiUrl);
+    return this.http
+      .get<MemberResponse[]>(this.membersApiUrl)
+      .pipe(
+        map(response => {
+          return response.map(memberResponse => new Member(memberResponse));
+        })
+      );
   }
 }
