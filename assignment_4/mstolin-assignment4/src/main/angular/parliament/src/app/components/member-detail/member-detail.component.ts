ea5b7f23b4f1aa8e6909d386ab50a1bdc1725362
Marcel Stolin <marcelstolin@gmail.com>
2021-11-24 06:58:40 +0100

    Updated detail component to parse data

--- assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/components/member-detail/member-detail.component.ts
+++ assignment_4/mstolin-assignment4/src/main/angular/parliament/src/app/components/member-detail/member-detail.component.ts
@@ -8,6 +8,18 @@ import {Website} from "../../models/website";
 import {Party} from "../../models/party";
 import {MemberParty} from "../../models/member-party";
 
+type Membership = {
+  memberParty: MemberParty
+  from: Date
+  until?: Date
+}
+
+type PartyMembership = {
+  party: Party
+  from: String
+  until: String
+}
+
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
@@ -19,45 +31,18 @@ export class MemberDetailComponent implements OnInit {
 
   member?: Member;
   websites: Website[] = [];
-  parties: Party[] = [];
+  partyMemberships: PartyMembership[] = [];
 
-  constructor(private route: ActivatedRoute, private dataService: DataCacheService) { }
+  constructor(private route: ActivatedRoute, private dataCacheService: DataCacheService) { }
 
   ngOnInit(): void {
     this.receiveMemberId();
 
     if (typeof this.memberId$ !== 'undefined') {
-      this.memberId$.subscribe(id => {
-        // get current member
-        this.dataService.members$!.pipe(
-          filter(member => member.id == id)
-        ).subscribe(member => this.member = member);
-
-        // get websites
-        this.dataService.websites$?.pipe(
-          filter(website => website.personId == id)
-        ).subscribe(website => this.websites.push(website));
-
-        // get parties
-        let memberParties = this.dataService.memberParties$?.pipe(
-          filter(memberParty => memberParty.personId == id), // only for current member
-          groupBy(memberParty => memberParty.partyId), // group duplicate parties ...
-          mergeMap(group =>
-            group.pipe(
-              toArray(),
-              map(groupedParties => this.mapPartyHistory(groupedParties))
-            )
-          ),
-          /*map(memberParty => {
-            // TODO: Map das in ein anderes model, sodass man noch das until und from anzeigen kann
-            // und danach sortieren
-            return this.getParty(memberParty.partyId);
-          })*/
-        ).subscribe(memberParty => {
-          this.dataService.parties$?.pipe(
-            filter(party => party.id == memberParty.partyId)
-          ).subscribe(party => this.parties.push(party));
-        });
+      this.memberId$.subscribe(memberId => {
+        this.receiveMember(memberId);
+        this.receiveWebsites(memberId);
+        this.receiveParties(memberId);
       });
     }
   }
@@ -71,15 +56,45 @@ export class MemberDetailComponent implements OnInit {
     );
   }
 
-  private mapPartyHistory(parties: MemberParty[]): MemberParty {
-    let first = parties[0];
+  private receiveMember(memberId: number): void {
+    this.dataCacheService.members$!.pipe(
+      filter(member => member.id == memberId)
+    ).subscribe(member => this.member = member);
+  }
+
+  private receiveWebsites(memberId: number) {
+    this.dataCacheService.websites$?.pipe(
+      filter(website => website.personId == memberId)
+    ).subscribe(website => this.websites.push(website));
+  }
+
+  private receiveParties(memberId: number) {
+    this.dataCacheService.memberParties$?.pipe(
+      filter(memberParty => memberParty.personId == memberId), // only for current member
+      groupBy(memberParty => memberParty.partyId), // group duplicate parties ...
+      mergeMap(group =>
+        group.pipe(
+          toArray(),
+          map(groupedParties => this.generateMembership(groupedParties)) // map to membership
+        )
+      )
+    ).subscribe(membership => {
+      this.dataCacheService.parties$?.pipe(
+        filter(party => party.id == membership.memberParty.partyId), // only parties of current member
+        map(party => this.generatePartyMembership(party, membership)) // generate partyMemberships
+      ).subscribe(partyMembership => this.partyMemberships.push(partyMembership)); // push to array
+    });
+  }
+
+  private generateMembership(parties: MemberParty[]): Membership {
+    let memberParty = parties[0];
 
     // get smallest from
     let from = parties
       .map(party => party.validFrom)
-      .reduce((previous, current) => {
-        return (previous < current) ? previous : current;
-      });
+      .reduce((previous, current) =>
+        (previous < current) ? previous : current
+      );
 
     // get highest until
     const now = new Date();
@@ -91,14 +106,28 @@ export class MemberDetailComponent implements OnInit {
           return now;
         }
       })
-      .reduce((previous, current) => {
-        return (previous < current) ? current : previous;
-      });
+      .reduce((previous, current) =>
+        (previous < current) ? current : previous
+      );
 
-    //first.ValidFromDate = from.toDateString();
-    //first.ValidUntilDate = until.toDateString();
+    return {
+      memberParty,
+      from,
+      until: until != now ? until: undefined
+    };
+  }
 
-    return first;
+  private generatePartyMembership(party: Party, membership: Membership): PartyMembership {
+    let from = this.getFormattedDate(membership.from);
+    let until = typeof membership.until !== 'undefined'
+      ? this.getFormattedDate(membership.until)
+      : 'Today';
+
+    return {party, from, until};
+  }
+
+  private getFormattedDate(date: Date): string {
+    return `${date.getMonth()}/${date.getFullYear()}`;
   }
 
 }
