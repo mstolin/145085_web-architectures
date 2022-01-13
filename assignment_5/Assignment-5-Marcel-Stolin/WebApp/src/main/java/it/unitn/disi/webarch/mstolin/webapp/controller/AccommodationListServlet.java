2c215d210b1073161d3b3a81d9a012cabf008015
Marcel Stolin <mstolin@mailbox.org>
2022-01-13 19:27:56 +0100

    Updated controller to use new factory

--- assignment_5/Assignment-5-Marcel-Stolin/WebApp/src/main/java/it/unitn/disi/webarch/mstolin/webapp/controller/AccommodationListServlet.java
+++ assignment_5/Assignment-5-Marcel-Stolin/WebApp/src/main/java/it/unitn/disi/webarch/mstolin/webapp/controller/AccommodationListServlet.java
@@ -1,53 +1,46 @@
 package it.unitn.disi.webarch.mstolin.webapp.controller;
 
 import it.unitn.disi.webarch.mstolin.dao.accommodation.AccommodationEntity;
+import it.unitn.disi.webarch.mstolin.dao.accommodation.ApartmentEntity;
+import it.unitn.disi.webarch.mstolin.dao.accommodation.HotelEntity;
+import it.unitn.disi.webarch.mstolin.webapp.servicelocator.ServiceFactory;
 import it.unitn.disi.webarch.mstolin.webservices.accommodations.AccommodationService;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class AccommodationListServlet extends HttpServlet {
 
+    private final Logger logger = Logger.getLogger(AccommodationListServlet.class.getName());
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        final Hashtable jndiProperties = new Hashtable();
-        jndiProperties.put(
-                Context.INITIAL_CONTEXT_FACTORY,
-                "org.wildfly.naming.client.WildFlyInitialContextFactory"
-        );
-        jndiProperties.put(
-                Context.PROVIDER_URL,
-                "http-remoting://localhost:8080"
-        );
-
-        Context ctx = null;
-        AccommodationService accommodationService = null;
-
         try {
-            ctx = new InitialContext(jndiProperties);
-            System.out.println("before");
-            accommodationService = (AccommodationService) ctx.lookup(
-                    "ejb:/WebServices.1.0-SNAPSHOT/AccommodationBean!it.unitn.disi.webarch.mstolin.webservices.accommodations.AccommodationService");
+            AccommodationService accommodationService = ServiceFactory.initializeService(
+                    ServiceFactory.ACCOMMODATION_BEAN,
+                    AccommodationService.class.getName()
+            );
             List<AccommodationEntity> allAccommodations = accommodationService.getAll();
 
             response.setContentType("text/html");
             PrintWriter writer = response.getWriter();
             writer.println("<html><body>");
             for(AccommodationEntity accommodation: allAccommodations) {
-                writer.println("<h1>" + accommodation.getName() + "</h1>");
+                if (accommodation instanceof ApartmentEntity) {
+                    writer.println("<a href=\"" + request.getContextPath() + "/accommodation/" + accommodation.getId() + "\">Apartment: " + accommodation.getName() + "</a><br/>");
+                } else if (accommodation instanceof HotelEntity) {
+                    writer.println("<a href=\"" + request.getContextPath() + "/accommodation/" + accommodation.getId() + "\">Hotel: " + accommodation.getName() + "</a><br/>");
+                }
             }
             writer.println("</body></html>");
-        } catch (NamingException ex) {
-            Logger.getLogger(AccommodationListServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException e) {
+            this.logger.log(Level.SEVERE, null, e);
         }
     }
 
--- assignment_5/Assignment-5-Marcel-Stolin/WebApp/src/main/java/it/unitn/disi/webarch/mstolin/webapp/controller/AccommodationPageServlet.java
+++ assignment_5/Assignment-5-Marcel-Stolin/WebApp/src/main/java/it/unitn/disi/webarch/mstolin/webapp/controller/AccommodationPageServlet.java
@@ -1,20 +1,14 @@
 package it.unitn.disi.webarch.mstolin.webapp.controller;
 
 import it.unitn.disi.webarch.mstolin.dao.accommodation.AccommodationEntity;
-import it.unitn.disi.webarch.mstolin.webapp.servicelocator.ServiceAddressBuilder;
-import it.unitn.disi.webarch.mstolin.webapp.servicelocator.ServiceLocator;
+import it.unitn.disi.webarch.mstolin.webapp.servicelocator.ServiceFactory;
 import it.unitn.disi.webarch.mstolin.webservices.accommodations.AccommodationService;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Hashtable;
-import java.util.List;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,13 +16,6 @@ import java.util.logging.Logger;
 public class AccommodationPageServlet extends HttpServlet {
 
     private final Logger logger = Logger.getLogger(AccommodationListServlet.class.getName());
-
-    // IN EINE FACTORY !!!
-    private AccommodationService getAccommodationServiceBean() throws NamingException {
-        String beanAddress = ServiceAddressBuilder.generateAccommodationBeanAddress(AccommodationService.class.getName());
-        AccommodationService accommodationService = ServiceLocator.getInstance().getService(beanAddress);
-        return accommodationService;
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -40,7 +27,10 @@ public class AccommodationPageServlet extends HttpServlet {
         this.logger.log(Level.SEVERE, "Requested Accommodation ID: " + id);
 
         try {
-            AccommodationService accommodationService = this.getAccommodationServiceBean();
+            AccommodationService accommodationService = ServiceFactory.initializeService(
+                    ServiceFactory.ACCOMMODATION_BEAN,
+                    AccommodationService.class.getName()
+            );
             AccommodationEntity accommodation = accommodationService.getAccommodation(id);
 
             response.setContentType("text/html");
