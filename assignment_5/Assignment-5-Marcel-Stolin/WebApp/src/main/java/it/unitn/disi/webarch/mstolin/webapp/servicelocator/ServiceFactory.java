b123b6f3af6da4442a4ae6fdf9c6c1da0c2ac194
Marcel Stolin <mstolin@mailbox.org>
2022-01-19 19:54:04 +0100

    Used the term service instead of bean

--- assignment_5/Assignment-5-Marcel-Stolin/WebApp/src/main/java/it/unitn/disi/webarch/mstolin/webapp/servicelocator/ServiceFactory.java
+++ assignment_5/Assignment-5-Marcel-Stolin/WebApp/src/main/java/it/unitn/disi/webarch/mstolin/webapp/servicelocator/ServiceFactory.java
@@ -9,13 +9,13 @@ public class ServiceFactory {
     public final static String ACCOMMODATION_BEAN = "AccommodationBean";
     public final static String RESERVATION_BEAN = "ReservationBean";
 
-    private static String generateBeanAddress(String beanName, String className) {
+    private static String generateServiceAddress(String beanName, String className) {
         return "ejb:/" + APP_NAME + "/" + beanName + "!" + className;
     }
 
     public static <T extends Object> T initializeService(String beanName, String className) throws NamingException {
-        final String beanAddress = generateBeanAddress(beanName, className);
-        T service = ServiceLocator.getInstance().getService(beanAddress);
+        final String serviceAddress = generateServiceAddress(beanName, className);
+        T service = ServiceLocator.getInstance().getService(serviceAddress);
         return service;
     }
 
