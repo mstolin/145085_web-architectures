f3c59004fffa5f04a03658a9a3a4fb6aa0c138a4
Marcel Stolin <mstolin@mailbox.org>
2022-01-20 20:16:45 +0100

    Updated ControlerHelper to use sql Date

--- assignment_5/Assignment-5-Marcel-Stolin/WebApp/src/main/java/it/unitn/disi/webarch/mstolin/webapp/helper/ControllerHelper.java
+++ assignment_5/Assignment-5-Marcel-Stolin/WebApp/src/main/java/it/unitn/disi/webarch/mstolin/webapp/helper/ControllerHelper.java
@@ -1,17 +1,12 @@
 package it.unitn.disi.webarch.mstolin.webapp.helper;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Date;
 
 public class ControllerHelper {
 
-    public static Date parseStringToDate(String dateString, String format) throws ParseException {
-        SimpleDateFormat formatter = new SimpleDateFormat(format);
-        return formatter.parse(dateString);
+    public static Date parseStringToDate(String dateString) throws ParseException {
+        return Date.valueOf(dateString);
     }
 
-    public static Date parseStringToDate(String dateString) throws ParseException {
-        return parseStringToDate(dateString, "yyyy-MM-dd");
-    }
 }
